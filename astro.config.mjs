2a15f09ab34b9caeb8ec12cfdd87bdba1988a04d
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2026-01-07 18:44:33 -0300

    [FEAT] Add NavBar and Title components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tubular-choux-34c96f.netlify.app',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 });
